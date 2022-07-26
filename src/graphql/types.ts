ba649d54dbdf912702fdce3cfc54c11415242617
Daniil Postnik <d.postnik1@gmail.com>
2022-07-26 14:57:21 +0300

    implemented mutations for patch card values, remove card

--- src/graphql/types.ts
+++ src/graphql/types.ts
@@ -1,7 +1,9 @@
 import {Card} from "./types/card";
 import {NewCard} from "./types/new-card";
+import {CardPatch} from "./types/card-patch";
 
 export const types = [
     Card,
     NewCard,
+    CardPatch,
 ]
